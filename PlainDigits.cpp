0a8ed548f305b7d7b7cc69c955843bd1f6fd9978
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-26 13:02:43 +0300

    Добавьте файлы проекта.

---
+++ PlainDigits.cpp
@@ -0,0 +1,76 @@
+﻿//  Написать функцию, возвращающую количество простых чисел в передаваемом ей массиве.
+
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+
+using namespace std;
+
+
+template <typename T>
+void FillArray(T arr[], int size);
+
+template <typename T>
+void PrintArray(T arr[], int size);
+
+
+template <typename T>
+int CountPlainsInArray(T arr[], int size);
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	const int sizze = 30;
+	int Array[sizze];
+
+	FillArray(Array, sizze);
+	cout << "Массив до предобразования :" << endl;
+	PrintArray(Array, sizze);
+		
+	cout << "В массиве простых чисел  : " << CountPlainsInArray(Array, sizze) << endl;	
+}
+
+
+template <typename T>
+void FillArray(T arr[], int size) {
+	random_device Random;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = Random() % (100 + 50) - 50;
+	}
+}
+
+template <typename T>
+void PrintArray(T arr[], int size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+template <typename T>
+int CountPlainsInArray(T arr[], int size) {
+	int count = 0;
+
+	for (int i = 0; i < size; ++i) {
+		bool isPlane = true;
+		for (int j = 2; j <= sqrt(arr[i]); ++j) {
+			if (arr[i] % j == 0) {
+				isPlane = false;
+				break;
+			}
+		}
+
+		if (isPlane) {
+			++count;
+		}
+	}
+
+	return count;
+}
